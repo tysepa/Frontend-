f7fd5b2d205f0741a7833091eee3d493fbd22ec1
tysepa <tuyiepa88@gmail.com>
2022-03-04 10:04:16 +0200

    fuction of crud

--- crud/create.js
+++ crud/create.js
@@ -1,22 +1,5 @@
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCx-U4VkwnRSZEdsuMbMJNYq5EjPEqGRIA",
-//     authDomain: "crud-de5f7.firebaseapp.com",
-//     projectId: "crud-de5f7",
-//     storageBucket: "crud-de5f7.appspot.com",
-//     messagingSenderId: "16546212598",
-//     appId: "1:16546212598:web:b2a36c5f2bc18dde4f5ad7"
-//   };
 
-//   firebase.initializeApp(firebaseConfig);
-// const db = firebase.firestore();
-
-
-// const modalWrapper = document.querySelector('.modal-wrapper');
-
-// const addModal = document.querySelector('.add-modal');
 const addModalFormTrue = document.querySelector('.contact-left .form');
-// const btnAns = document.querySelector('.btn-ans');
-// const tableUsers = document.querySelector('.table-users');
 
 
 
